c5bf02aa92ef98b69851bf71cb1c519a79d271e6
piezoh <noriko.piezoh@gmail.com>
2023-07-05 20:44:47 +0900

    C-2

--- customer.py
+++ customer.py
@@ -1,14 +1,18 @@
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def full_name(self):
         return f"{self.first_name} {self.family_name}"
 
 
-ken = Customer(first_name="Ken", family_name="Tanaka")
-ken.full_name()  # "Ken Tanaka" という値を返す
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.age  # 15 という値を返す
 
-tom = Customer(first_name="Tom", family_name="Ford")
-tom.full_name()  # "Tom Ford" という値を返す
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.age  # 57 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.age  # 73 という値を返す
